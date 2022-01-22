00b5c8b7c9ed47052336f293440295f1f5d94f03
Pawel <blachut.pawel@gmail.com>
2022-01-22 17:01:28 +0100

    Update user-management scripts

--- home-active-manager-parent/user-management-module/scripts/start_app.sh
+++ home-active-manager-parent/user-management-module/scripts/start_app.sh
@@ -4,13 +4,20 @@ app_name="user-management-module"
 profile="prod"
 
 path=$(dirname "$0")
+profile="prod"
 
 if [[ $1 == "dev" ]];then
   profile="dev"
+  source ./${path}/dev.config
+else
+  source ./${path}/prod.config
 fi
 
 echo "Start ${app_name} application with profile ${profile}"
 
 java_args=("-Dspring.profiles.active=${profile}")
+java_args=("-Dspring.datasource.url=${DATASOURCE_URL}")
+java_args+=("-Dspring.datasource.username=${DATASOURCE_USERNAME}")
+java_args+=("-Dspring.datasource.password=${DATASOURCE_PASSWORD}")
 
 sudo java -jar ${java_args[@]} ${path}/${app_name}-*.jar &
