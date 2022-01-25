beb00d1128f4765cb09c1c40faae7c5e7ea9c5e6
Pawel <blachut.pawel@gmail.com>
2022-01-25 23:15:23 +0100

    pr test

--- home-active-manager-parent/user-management-module/src/main/java/com/pawbla/project/home/user/management/configurations/DbConfiguration.java
+++ home-active-manager-parent/user-management-module/src/main/java/com/pawbla/project/home/user/management/configurations/DbConfiguration.java
@@ -2,10 +2,8 @@ package com.pawbla.project.home.user.management.configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.domain.   EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
