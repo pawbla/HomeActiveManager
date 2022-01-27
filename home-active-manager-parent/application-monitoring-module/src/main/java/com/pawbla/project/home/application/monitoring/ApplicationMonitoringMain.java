5ad1a21f65085443d34c9c1256a41c14b2693a2d
Pawel <blachut.pawel@gmail.com>
2022-01-27 22:39:34 +0100

    Application monitoring module

---
+++ home-active-manager-parent/application-monitoring-module/src/main/java/com/pawbla/project/home/application/monitoring/ApplicationMonitoringMain.java
@@ -0,0 +1,13 @@
+package com.pawbla.project.home.application.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMonitoringMain
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApplicationMonitoringMain.class, args);
+    }
+}
